8ac6d61dc1afae340f724ecd00fa996fd269780d
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-08 19:13:56 +0200

    sc

--- 06-nextjs-nginx/pages/index.js
+++ 06-nextjs-nginx/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -17,8 +18,8 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        Go to Other Page{' '}
+          <Link href="/demo">Demo</Link>
         </p>
 
         <div className={styles.grid}>
